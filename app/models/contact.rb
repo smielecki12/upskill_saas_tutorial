023eabb4cc211ecf8fc29e408267c9df2f5d1971
Matthew Smielecki <smielecki12@gmail.com>
2017-11-30 18:40:46 +0000

    Improved flash style

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,7 @@ class Contact < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
   validates :comments, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :comments, length: { maximum: 500 }
+  {  too_long: "%{500} characters is the maximum allowed" }
 end
